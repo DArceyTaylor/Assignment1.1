b159340281e4fa072b390487af30e6cc3e10a4dc
D'Arcey Taylor <darceytaylor05@gmail.com>
2016-02-05 02:53:25 -0500

    testing awesome css

--- routes/index.js
+++ routes/index.js
@@ -7,19 +7,19 @@ router.get('/', function(req, res, next) {
 });
 /* GET About page. */
 router.get('/About', function(req, res, next) {
-  res.render('about', { title: 'About Me' });
+  res.render('index', { title: 'About Me' });
 });
 /* GET Projects page. */
 router.get('/Projects', function(req, res, next) {
-  res.render('projects', { title: 'My Projects' });
+  res.render('index', { title: 'My Projects' });
 });
 /* GET Services page. */
 router.get('/Services', function(req, res, next) {
-  res.render('services', { title: 'Services' });
+  res.render('index', { title: 'Services' });
 });
 /* GET Contact Me page. */
 router.get('/Contact', function(req, res, next) {
-  res.render('contact', { title: 'Contact Me' });
+  res.render('index', { title: 'Contact Me' });
 });
 
 module.exports = router;
